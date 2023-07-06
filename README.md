c9706bc28b4d91a25bef524727ec4b49bd3e1ab1
govindgupta9 <137889597+govindgupta9@users.noreply.github.com>
2023-07-07 00:11:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-xavx7z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-xavx7z)
---
+++ index.js
@@ -0,0 +1,7 @@
+let a=100;
+for(let i=1; i<=a;i++){
+    if(i%3==0){console.log("google");}
+    else if(i%5==0){console.log("facebook");}
+   else if(i%3==0 && i%5==0){console.log("amazon");}
+    else{console.log(i);}
+}
